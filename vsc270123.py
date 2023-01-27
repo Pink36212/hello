40909997cfd3e3bfcd9efc25579639a88090cd16
Pink36212 <112546213+Pink36212@users.noreply.github.com>
2023-01-27 15:39:18 +0700

    Add files via upload

---
+++ vsc270123.py
@@ -0,0 +1,5 @@
+total = 0
+for i in range(10):
+    answer=int("Enter the number") 
+    total=total + answer
+print(total)                                                                                                                                                                                                                                                                                                                                                                                                                                                     
